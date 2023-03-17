a32823b7e2fa6dc07861ce2164b6124de1a70a63
SpecXI <geoleemah@gmail.com>
2023-03-16 18:16:07 -0700

    Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
- * main - Writes a C program that prints exactly and that piece of art is useful
+ * main - Write a C program that prints exactly and that piece of art is useful
  * Return: Always 1.
  */
 int main(void)
