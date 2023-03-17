4f548c84cbe70a79cd653d86ce4830e4ee84084f
SpecXI <geoleemah@gmail.com>
2023-03-16 17:20:07 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - entry point
- * return: Always 0
+ * main - prints "Programming is like building a multilingual puzzle."`
+ * Return: Always 0.
  */
 int main(void)
 {
