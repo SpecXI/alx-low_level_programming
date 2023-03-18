d369c025b91db6df261cb9379c1355ebcf43a629
SpecXI <geoleemah@gmail.com>
2023-03-17 18:44:56 -0700

    i hope this works

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints lowercase and uppercase alphabets
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+
+	for (low = 'A'; low <= 'Z'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+}
