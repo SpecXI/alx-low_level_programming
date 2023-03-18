f57b57fdcde45a7899e6efc16a71a6616c617666
SpecXI <geoleemah@gmail.com>
2023-03-17 18:28:28 -0700

    Im enjoying this more than i should be

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet
+ * Return: Always (Successs)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
