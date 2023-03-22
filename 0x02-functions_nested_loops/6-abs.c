ceb470da367b5a554aa2978e86b74deec540ab88
SpecXI <geoleemah@gmail.com>
2023-03-22 10:22:49 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the ablsolutr value of an integer
+ * @c: The number to be computed
+ * Return: Absolutr of numer or zero
+ */
+
+int _abs(int c)
+
+{
+
+	if (c < 0)
+	{
+	int abs_val;
+
+	abs_vaal = c * -1;
+	return (abs_val);
+	}
+	return (c);
+}
