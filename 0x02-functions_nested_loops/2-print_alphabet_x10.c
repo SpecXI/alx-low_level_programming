63f9a5e5a4a275c6f1a92aa35b93dcb265cdb495
SpecXI <geoleemah@gmail.com>
2023-03-22 09:33:30 -0700

    Nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,12 @@
  * Return: void
  */
 void print_alphabet_x10(void)
+
 {
+
 	char c;
 	int i = 0;
+
 	while (i <= 9)
 	{
 	for (c = 'a'; c <= 'z'; c++)
@@ -18,4 +21,5 @@ void print_alphabet_x10(void)
 	_putchar('\n');
 	i++;
 	}
+
 }
