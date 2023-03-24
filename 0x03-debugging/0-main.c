c7976e19740e9700ba715e0032727fd98e9f372d
SpecXI <geoleemah@gmail.com>
2023-03-24 16:03:26 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - function prints positive or negative integers
+ * Return: 0
+ */
+int main(void)
+
+{
+	int;
+
+	i=0;
+	positive_or_negative(i);
+	return (0);
+}
