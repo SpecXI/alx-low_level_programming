cc03a3616962a954c004e8fde954ff6f1204535e
SpecXI <geoleemah@gmail.com>
2023-03-24 20:22:59 -0700

    nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print num from 0 to 9
+ * Return: The num from 0 yo 9
+ */
+
+void print_numbers(void)
+
+
+{
+	int a
+	for (a = 0; a <= 9; a++)
+	{
+	_putchar(a + '0);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
+int _isupper(int x);
+int _isdigit(int x);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
