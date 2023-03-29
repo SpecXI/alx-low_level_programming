c28204b94b911f9523aa622698bffae385f9dbe3
SpecXI <geoleemah@gmail.com>
2023-03-28 18:43:59 -0700

    pointers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
  * _puts - Prints a string
@@ -6,7 +6,7 @@
  * Return: void
  */
 
-void puts(char *str)
+void _puts(char *str)
 {
 	for (; *str != '\0'; str++)
 	{
