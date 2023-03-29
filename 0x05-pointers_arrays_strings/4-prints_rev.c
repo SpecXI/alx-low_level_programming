f6e40c620b8b6cc353124c3616de864861eacf5c
SpecXI <geoleemah@gmail.com>
2023-03-28 19:16:12 -0700

    Arrays

---
+++ 0x05-pointers_arrays_strings/4-prints_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio>
+
+/**
+ * print_rev - Prints str in reverse
+ * @s: The str to print
+ * Return: void
+ */
+
+void print_rev(char *s)
+
+{
+
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+	c++;
+	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+	_putchar(s[c]);
+	}
+	_putchar('\n');
+}
