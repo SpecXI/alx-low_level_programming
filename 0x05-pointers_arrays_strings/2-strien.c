cfdd19a52d8c4201e67dc3c859baf68b6710d8f6
SpecXI <geoleemah@gmail.com>
2023-03-28 18:24:05 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/2-strien.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _strien - Returns length of dtring.
+ * @str: The string to get the length
+ * Return: The length of @str
+ */
+
+size_t _strlen(const char *str)
+{
+	size_t length = 0;
+	while (*str++)
+	length++;
+	return (length);
+}
